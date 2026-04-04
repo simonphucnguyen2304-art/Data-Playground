6387d228dec49ed3062a3fa65f7057d9310354d8
Huỳnh Phúc Nguyên <simonsimonphucnguyen2304@gmail.com>
2026-04-04 15:50:28 +0700

    Update "10 more questions"

--- awesome-chocolate/source.sql
+++ awesome-chocolate/source.sql
@@ -45,3 +45,66 @@ JOIN people ON sales.SPID = sales.SPID;
 SELECT sales.*, geo.Region
 FROM sales
 JOIN geo ON sales.GeoID = geo.GeoID;
+
+# Câu 11: Hiển thị thông tin giao dịch kèm theo chi phí trên mỗi box.
+SELECT sales.*, products.Cost_per_box
+FROM sales
+JOIN products ON sales.PID = products.PID;
+
+# Câu 12: Xem các giao dịch cùng với cả thông tin sản phẩm và người bán.
+SELECT s.*, p.Product, p.Category, p.Size, p.Cost_per_box,
+		pe.Salesperson, pe.Team
+FROM sales s
+JOIN products p ON s.PID = p.PID 
+JOIN people pe ON s.SPID = pe.SPID;
+
+# Câu 13: Tính tổng doanh thu của toàn bộ hệ thống.
+SELECT SUM(Amount) AS TotalRevenue
+FROM Sales;
+
+# Câu 14: Tính giá trị trung bình của mỗi giao dịch.
+SELECT AVG(Amount) AS AverageRevenue
+FROM Sales;
+
+# Câu 15: Đếm số lượng giao dịch theo từng khu vực.
+SELECT g.Region, COUNT(*) AS TransactionCount
+FROM sales s
+JOIN geo g ON s.GeoID = g.GeoID
+GROUP BY g.Region
+
+# Câu 16: Tính tổng số khách hàng được phục vụ bởi mỗi nhân viên bán hàng.
+SELECT pe.Salesperson, SUM(s.Customers) AS TotalCustomers
+FROM sales s 
+JOIN people pe ON s.SPID = pe.SPID
+GROUP BY pe.Salesperson; 
+
+# Câu 17: Những nhân viên nào có tổng doanh thu lớn hơn 1600000?
+SELECT pe.Salesperson, SUM(s.Amount) AS TotalRevenue
+FROM sales s 
+JOIN people pe ON s.SPID = pe.SPID
+GROUP BY pe.Salesperson
+HAVING SUM(s.Amount) > 1600000
+
+# Câu 18: Những sản phẩm nào được bán với tổng số lượng box lớn hơn 500?
+SELECT p.Product, SUM(s.Boxes) AS TotalBoxes
+FROM sales s
+JOIN products p ON s.PID = p.PID
+GROUP BY p.Product
+HAVING SUM(s.Boxes) > 500
+
+# Câu 19: Tìm 5 sản phẩm có doanh thu cao nhất.
+SELECT p.Product, SUM(s.Amount) AS TotalRevenue
+FROM sales s 
+JOIN products p ON s.PID = p.PID
+GROUP BY p.Product
+ORDER BY TotalRevenue DESC
+LIMIT 5; 
+
+# Câu 20: Tìm 3 khu vực có doanh thu cao nhất, sắp xếp theo thứ tự giảm dần.
+SELECT g.Region, SUM(s.Amount) AS TotalRevenue
+FROM sales s
+JOIN geo g ON s.GeoID = g.GeoID
+GROUP BY g.Region 
+ORDER BY TotalRevenue DESC
+LIMIT 3;
+
